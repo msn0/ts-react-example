092c4ac25a76a473d933ca595a5a9db07aa78195
Michał Jezierski <ambinanitelo@gmail.com>
2019-01-16 14:46:33 +0100

    export loaders

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,60 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractCssChunks = require('extract-css-chunks-webpack-plugin');
 
+const Loaders = {
+    babel: {
+        loader: 'babel-loader',
+        options: {
+            presets: [
+                ['@babel/preset-env', { modules: false }],
+                '@babel/preset-react'
+            ]
+        }
+    },
+    typescript: {
+        loader: 'awesome-typescript-loader'
+    },
+    cssExtract: ExtractCssChunks.loader,
+    cssTypescript: {
+        loader: 'css-modules-typescript-loader'
+    },
+    cssModules: {
+        loader: 'css-loader',
+        options: {
+            modules: true
+        }
+    },
+    postcss: {
+        loader: 'postcss-loader',
+        options: {
+            plugins: () => [
+                require('postcss-import-sync2'),
+                require('postcss-at-rules-variables'),
+                require('postcss-mixins'),
+                require('postcss-modular-scale-plus'),
+                require('postcss-simple-vars'),
+                require('postcss-custom-properties'),
+                require('postcss-remove-null'),
+                require('postcss-nested'),
+                require('postcss-calc'),
+                require('postcss-custom-media')
+            ]
+        }
+    }
+}
+
+module.exports.Loaders = Loaders;
+
 module.exports = {
     entry: path.resolve(__dirname, 'src/index'),
     output: {
         path: path.resolve(__dirname, 'dist/'),
-        filename: 'index.bundle.js',
+        filename: 'app.bundle.js',
         libraryTarget: 'var',
         publicPath: ''
     },
     target: 'web',
-    mode: 'development',
+    mode: 'production',
     resolve: {
         extensions: ['.ts', '.tsx', '.js']
     },
@@ -19,64 +63,19 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: [
-                                ['@babel/preset-env', { modules: false }],
-                                '@babel/preset-react'
-                            ]
-                        }
-                    }
-                ]
+                use: [ Loaders.babel ]
             },
             {
                 test: /\.tsx?$/,
-                loader: 'awesome-typescript-loader'
+                use: [ Loaders.typescript ]
             },
             {
                 test: /\.pcss$/,
                 use: [
-                    ExtractCssChunks.loader,
-                    {
-                        loader: 'css-modules-typescript-loader'
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true
-                        }
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: () => [
-                                require('postcss-import-sync2'),
-                                require('postcss-at-rules-variables'),
-                                require('postcss-mixins'),
-                                require('postcss-modular-scale-plus'),
-                                require('postcss-simple-vars'),
-                                require('postcss-custom-properties'),
-                                require('postcss-remove-null'),
-                                require('postcss-nested'),
-                                require('postcss-calc'),
-                                require('postcss-custom-media')
-                            ]
-                        }
-                    }
-                    // {
-                    //     // loader: 'css-modules-typescript-loader',
-                    //     // loader: 'typed-css-modules-loader',
-                    //     // loader: 'typings-for-css-modules-loader',
-                    //     // loader: 'css-loader',
-                    //     options: {
-                    //         modules: true,
-                    //         camelCase: true
-                    //         // namedExport: true
-                    //         // exportOnlyLocals: true
-                    //     }
-                    // }
+                    Loaders.cssExtract,
+                    Loaders.cssTypescript,
+                    Loaders.cssModules,
+                    Loaders.postcss
                 ]
             }
         ]
