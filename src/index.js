4be288601c8e35ab02ddca161c052b739d9e684b
Michał Jezierski <ambinanitelo@gmail.com>
2019-01-17 15:00:33 +0100

    make entry a good old js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 import BadgeFunctionalTs from './components/badge-functional-ts/badge-functional';
 import BadgeFunctionalJs from './components/badge-functional-js/badge-functional';
